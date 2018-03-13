53ee0d0154fb25722cc08b9bfd9de830551e22aa
Ross Hill <rosshill88@gmail.com>
2018-03-13 20:35:12 +0000

    Created constructor for comment box

--- comments/src/containers/CommentBox.js
+++ comments/src/containers/CommentBox.js
@@ -2,6 +2,16 @@ import React, {Component} from 'react';
 import CommentList from './../components/CommentList';
 
 class CommentBox extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+       data: [{ id: 1, author: "Beth", text: "I love cats!" }]
+    }
+  }
+
+
+
   render() {
     return (
       <div className="commentBox">
